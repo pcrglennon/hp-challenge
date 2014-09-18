feacb4256c7c830665d2cff1558842a097fd44bf
pcrglennon <pcrglennon@gmail.com>
2014-09-18 16:35:24 -0400

    Implement User#rentals_as_renter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  has_many :bikes, foreign_key: 'owner_id'
+  has_many :bikes, foreign_key: "owner_id"
+
+  def rentals_as_renter
+    self.class.joins("INNER JOIN rentals ON rentals.renter_id = users.id")
+      .where(rentals: {renter_id: self.id})
+  end
 end
