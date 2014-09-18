10d995ba6a9d1d722e51ee1b7763bebdb3215030
pcrglennon <pcrglennon@gmail.com>
2014-09-18 19:48:02 -0400

    Working version of User#rentals_as_owner

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   has_many :bikes, foreign_key: "owner_id"
 
   def rentals_as_owner
-
+    Rental.joins("INNER JOIN bikes ON bikes.id = rentals.bike_id")
+      .where(bike_id: [self.bike_ids])
   end
 
   def rentals_as_renter
