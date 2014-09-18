e304098183a309eae01617385d78dd82e0d8dc65
pcrglennon <pcrglennon@gmail.com>
2014-09-18 19:11:40 -0400

    Fix completely awful User#rentals_as_renter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
   has_many :bikes, foreign_key: "owner_id"
 
+  def rentals_as_owner
+
+  end
+
   def rentals_as_renter
-    self.class.joins("INNER JOIN rentals ON rentals.renter_id = users.id")
+    Rental.joins("INNER JOIN users ON users.id = rentals.renter_id")
       .where(rentals: {renter_id: self.id})
   end
 end
