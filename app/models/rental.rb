0d8fb0c3293d6f789cb47418f915d66ac116a6a2
pcrglennon <pcrglennon@gmail.com>
2014-09-18 16:24:02 -0400

    Implement Rental#status and Rental#owner

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,19 @@
 class Rental < ActiveRecord::Base
   belongs_to :renter, class_name: "User"
   belongs_to :bike
+
+  def status
+    today = Date.today
+    if end_date < today
+      "Completed"
+    elsif start_date <= today && end_date >= today
+      "Current"
+    else
+      "Upcoming"
+    end
+  end
+
+  def owner
+    bike.owner
+  end
 end
