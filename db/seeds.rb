0fba8e82378072929a56061a8e84e31dd4b204a6
pcrglennon <pcrglennon@gmail.com>
2014-09-20 13:23:21 -0400

    Boost the numbers in seeds again

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,28 @@
 require 'faker'
 
-def seed_users
-  60.times.map do |i|
+puts "Seeding begun\n"
+
+def seed_users(count)
+  puts "Seeding #{count} users"
+  count.times.map do |i|
     User.create(name: Faker::Name.name)
   end
 end
 
-users = seed_users
+users = seed_users(100)
 puts "User seed data completed"
 
-def seed_bikes(users)
+def seed_bikes(users, count)
+  puts "Seeding #{count} bikes"
   colors = %w(red orange yellow green blue indigo violet)
   categories = %w(road mountain hybrid bmx recumbent stationary penny-farthing)
 
-  200.times.map do |i|
+  count.times.map do |i|
     Bike.create(owner: users.sample, color: colors.sample, category: categories.sample)
   end
 end
 
-bikes = seed_bikes(users)
+bikes = seed_bikes(users, 400)
 puts "Bike seed data completed"
 
 def rental_dates
@@ -32,12 +36,14 @@ def rental_dates
   {start_date: start_date, end_date: end_date}
 end
 
-def seed_rentals(users, bikes)
-  1000.times.map do |i|
+def seed_rentals(users, bikes, count)
+  puts "Seeding #{count} rentals"
+  count.times.map do |i|
+    puts "#{i}/#{count} rentals created" if i > 0 && i % 1000 == 0
     dates = rental_dates
     Rental.create(renter: users.sample, bike: bikes.sample, start_date: dates[:start_date], end_date: dates[:end_date])
   end
 end
 
-seed_rentals(users, bikes)
+seed_rentals(users, bikes, 5000)
 puts "Rental seed data completed"
