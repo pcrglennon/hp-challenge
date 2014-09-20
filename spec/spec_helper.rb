448acbf21590b67750a863713456240341b3e0bc
pcrglennon <pcrglennon@gmail.com>
2014-09-20 16:28:14 -0400

    When running specs, use test DB

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,10 @@ require 'pry'
 
 RSpec.configure do |config|
 
+  # Use separate test DB
+  ActiveRecord::Base.establish_connection(CONNECTION_DETAILS["test"])
+  ActiveRecord::Migrator.migrate("db/migrate/")
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
